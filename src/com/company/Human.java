dd51c75ed6828f20bf274d028ebeddade5eeeed3
dudos <konstantyncz@gmail.com>
2021-02-17 13:17:52 +0100

    Zadanie 0 - wstępna konfiguracja

---
+++ src/com/company/Human.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import com.company.devices.Phone;
+
+public class Human {
+    public String firstName;
+    public String lastName;
+    protected Phone mobile;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.creatures.Animal;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Animal dog = new Animal();
+        dog.name = "Rex";
+
+        Human me = new Human();
+        me.firstName = "Kostiantyn";
+        me.lastName = "Chumak";
+    }
+}

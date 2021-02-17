74cab127e5923d04e40f0482658e9636c98b662e
dudos <konstantyncz@gmail.com>
2021-02-17 13:20:50 +0100

    Zadanie 1 - Pets feeding process

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import com.company.devices.Phone;
+import com.company.creatures.Animal;
 
 public class Human {
     public String firstName;
     public String lastName;
     protected Phone mobile;
+    public Animal pet;
 }
--- src/com/company/creatures/Animal.java
+++ src/com/company/creatures/Animal.java
@@ -1,5 +1,54 @@
 package com.company.creatures;
 
 public class Animal {
+    final static Double DEFAULT_WEIGHT_DOG = 4.0;
+    final static Double DEFAULT_WEIGHT_LION = 190.0;
+    final static Double DEFAULT_WEIGHT_MOUSE = 0.01;
+
     public String name;
+
+    final String species;
+    private Double weight;
+
+    public Animal(String species) throws Exception {
+        this.species = species;
+
+        switch (this.species) {
+            case "Mouse":
+                weight = DEFAULT_WEIGHT_MOUSE;
+                break;
+            case "Dog":
+                weight = DEFAULT_WEIGHT_DOG;
+                break;
+            case "Lion":
+                weight = DEFAULT_WEIGHT_LION;
+            default:
+                throw new Exception("Undefined species!");
+        }
+    }
+
+    public void feed() {
+        if (weight <= 0) {
+            System.out.println("Too late...");
+        } else {
+            weight++;
+            System.out.println("Thx, yummy.    Weight: " + weight);
+        }
+    }
+
+    public void takeForAWalk() {
+        if (weight <= 0) {
+            System.out.println("Too late...");
+        } else if (weight <= 3) {
+            weight--;
+            if (weight <= 0) {
+                System.out.println("Thx, nice walk, but i'm dying...");
+            } else {
+                System.out.println("Thx, nice walk, but i'm hungry.    Weight: " + weight);
+            }
+        } else {
+            weight--;
+            System.out.println("Thx, nice walk.    Weight: " + weight);
+        }
+    }
 }
